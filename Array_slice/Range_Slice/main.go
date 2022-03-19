c8515f358d703352a3eb012170206612f22f3136
JohnHM1 <serverz0622@gmail.com>
2022-03-19 00:24:18 -0500

    recorrido con range y demas fuciones

---
+++ Array_slice/Range_Slice/main.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func main() {
+
+	var slice = []string{"hola", "que", "hace"}
+
+	//La funcion range nos permitira ver dos conceptos de nuestros slice, Listas y Arrays; La posicion y el contenido de esa posicion.
+
+	var elemento int
+	var contenido string
+
+	for elemento, contenido = range slice {
+
+		fmt.Println(elemento, contenido)
+
+	}
+	//Ejecutando la Funcion.
+	isPalindromoUpperLower("Amor")
+}
+//
+// Actividad ; Detectar si la palabra es palindroma o no.
+func isPalindromo(text string) {
+
+	var textReverse string
+	// La i tomara el valor del numero de caracteres que tenga la palabra.
+	// Se le resta 1 con el proposito de contabilizar la casilla numero 0.
+	for i := len(text) - 1; i >= 0; i-- {
+		//se le asigna a la variable el caracter que se encuentre en la casilla numero i. Se transforma de esta manera el codigo ascii.
+		textReverse += string(text[i])
+	}
+	//si la palabra ingresada es igual a la variable : true, si no , false.
+
+	if text == textReverse {
+
+		fmt.Println(true)
+
+	} else {
+
+		fmt.Println(false)
+
+	}
+
+}
+
+func isPalindromoUpperLower(text string) {
+	//Con la funcion strings.To(Lower,Upper) se modifica el codigo ascii. (Mayus/Minus).
+	text = strings.ToLower(text)
+	var  textoReversa string
+	for i := len(text)-1 ; i >= 0 ; i--{
+
+		textoReversa += string(text[i])
+
+	}
+	if text == textoReversa {
+
+		fmt.Println(true)
+
+	}else{
+
+		fmt.Println(false)
+
+	}
+
+}
