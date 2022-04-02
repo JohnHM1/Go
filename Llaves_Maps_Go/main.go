6c8d6651a434b799b175a6aa172d089ab10fb8a7
JohnHM1 <serverz0622@gmail.com>
2022-04-01 19:47:04 -0500

    Llaves y Maps con Go

--- Llaves_Maps_Go/main.go
+++ Llaves_Maps_Go/main.go
@@ -23,4 +23,10 @@ func main() {
 
 	}
 
+	//Encontrar un valor 
+// Agregando una variable en la busqueda de la llavame en el map nospermitira saber si esa llave existe o no
+// imprimiendo un True si existe o un false en su defecto.
+	value, oki := m["Jose"]
+	fmt.Println(value,oki)
+
 }
